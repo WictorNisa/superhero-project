81ee849be766765663a89e07e42771e690976508
adhSwede <119529943+adhSwede@users.noreply.github.com>
2025-01-28 09:09:14 +0100

    Updated Home page + added details to home.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,48 +1,32 @@
-import styles from "./Home.module.css";
-import { fetchRandomFromAPI } from "../../services/ApiSearchFetch";
 import { useEffect, useState } from "react";
+import RandomCard from "../../components/RandomCard/RandomCard";
+import { fetchRandomFromAPI } from "../../services/ApiSearchFetch";
+import styles from "./Home.module.css";
 
-// Define the type for the superhero data
 interface HeroData {
+  id: string;
+  name: string;
   image: {
     url: string;
   };
-  name: string;
 }
 
 const Home = () => {
-  // State for fetched data, HeroData : null
   const [heroData, setHeroData] = useState<HeroData | null>(null);
 
-  // Function for calling Random Fetch
   const fetchData = async () => {
     const data: HeroData = await fetchRandomFromAPI();
     setHeroData(data);
   };
 
-  // Call on Load
   useEffect(() => {
     fetchData();
   }, []);
 
-  // Call on Button click
-  const handleButtonClick = () => {
-    fetchData();
-  };
-
   return (
     <section className={styles.HomeContainer}>
-      <div className={styles.HomeContainerInner}>
-        <h1> Welcome to the home page about superheroes</h1>
-        <div className={styles.imageWrapper}>
-          {/* Render image if no problems */}
-          {heroData && heroData.image && (
-            <img src={heroData.image.url} alt={heroData.name || "Superhero"} />
-          )}
-        </div>
-        <h2 className={styles.HomeContainerH2}>{heroData?.name}</h2>
-        <button onClick={handleButtonClick}>Give me another Hero!</button>
-      </div>
+      <h1>Welcome to the home page about superheroes</h1>
+      {heroData && <RandomCard hero={heroData} onFetchNewHero={fetchData} />}
     </section>
   );
 };
