11609ddbb55abbdb8702ddeed87ff46b4aae5060
adhSwede <119529943+adhSwede@users.noreply.github.com>
2025-02-01 12:36:28 +0100

    Favorites function added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.tsx";
+import Home from "./pages/Home/Home";
 import "./index.css";
-import App from "./App.tsx";
-import SuperHeroPage from "./pages/SuperHeroPage/SuperHeroPage.tsx";
-import Favorites from "./pages/Favorites/FavoritesPage.tsx";
-import { FavoritesProvider } from "./contexts/FavoritesContext.tsx"; // adjust the path if needed
+import App from "./App";
+import SuperHeroPage from "./pages/SuperHeroPage/SuperHeroPage";
+import Favorites from "./pages/Favorites/FavoritesPage";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
 
 createRoot(document.getElementById("root")!).render(
   <FavoritesProvider>
